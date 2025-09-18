25fd4fbb96d817fa99ad72e387e88a0e8dbb07a7
ceelestin <celest1.eve@gmail.com>
2025-09-18 15:20:14 +0200

    add config with 10 seeds and 10 splits

--- launch.sh
+++ launch.sh
@@ -12,6 +12,6 @@
 module purge
 module load arch/h100 pytorch-gpu
 
-params=$(awk -v  idx_param="${SLURM_ARRAY_TASK_ID}" 'NR==idx_param' configs.txt)
+params=$(awk -v  idx_param="${SLURM_ARRAY_TASK_ID}" 'NR==idx_param' configs2.txt)
 
 python pcam_deep_training.py $params
